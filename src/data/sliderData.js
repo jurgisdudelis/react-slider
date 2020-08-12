ffbe221f002167f342e8246504179d50deb55c30
jurgisdudelis <63905386+jurgisdudelis@users.noreply.github.com>
2020-08-12 20:23:06 +0300

    Updated _settings.scss component

--- src/data/sliderData.js
+++ src/data/sliderData.js
@@ -3,27 +3,27 @@ import React from 'react';
 const sliderData = [
   {
     content: "Item One",
-    description: "Here is a short description of the content"
+    description: "Here is a short description of the Item One"
   },
   {
     content: <img src="https://www.athmin.com/img/hiredeveloper/gif/react.gif" alt="React Gif" width="200" height="200"></img>,
-    description: "React gif using <img> attribute"
+    description: "React Gif"
   },
   {
-    content: "Item Three",
-    description: "This is Item Three"
+    content: <a href="https://reactjs.org/">Go To ReactJs</a>,
+    description: "External link"
   },
   {
-    content: <iframe width="311" height="200" src="https://www.youtube.com/embed/gx4rsZkCCss" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
-    description: "Embedded video using <iframe> attribute"
+    content: <iframe width="355" height="200" src="https://www.youtube.com/embed/gx4rsZkCCss" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>,
+    description: "Embedded Video"
   },
   {
     content: "Item Five",
-    description: "This is Item Five"
+    description: "Information about the item Five"
   },
   {
-    content: <a href="https://reactjs.org/">reactjs.org</a>,
-    description: "External link using <a> attribute"
+    content: "Item Six",
+    description: "This and that about Item Six"
   }
 ];
 
