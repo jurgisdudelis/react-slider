ec5786fa2bb4d5b80584c452f80a9abc3761a5a0
jurgisdudelis <63905386+jurgisdudelis@users.noreply.github.com>
2020-09-08 15:23:50 +0300

    Improved background image quality

--- src/data/sliderData.js
+++ src/data/sliderData.js
@@ -4,28 +4,28 @@ const sliderData = [
   {
     content: "Item One",
     description: "Here is a short description of the Item One",
-    img: <img src="https://cdn.pixabay.com/photo/2018/06/16/16/17/road-3478977_960_720.jpg" 
+    img: <img src="https://chromethemer.com/wallpapers/chromebook-wallpapers/download/mountain-hills-3840x2160.jpg" 
     alt="Item One">
     </img>
   },
   {
     content: "Item Two",
     description: "Here is a short description of the Item Two",
-    img: <img src="https://cdn.pixabay.com/photo/2013/05/28/20/30/city-114290_960_720.jpg" 
+    img: <img src="https://i.pinimg.com/originals/69/a0/57/69a057b0085c3c98cbff4e05605b2e96.jpg" 
     alt="Item Two">
     </img>
   },
   {
     content: "Item Three",
     description: "Here is a short description of the Item Three",
-    img: <img src="https://cdn.pixabay.com/photo/2014/12/08/02/59/bench-560435_960_720.jpg" 
+    img: <img src="https://www.geoffreykentsafari.com/wp-content/uploads/2018/10/bench-in-the-autumn.jpg" 
     alt="Item Three">
     </img>
   }, 
   {
     content: "Item Four",
     description: "Here is a short description of the Item Four",
-    img: <img src="https://cdn.pixabay.com/photo/2014/05/02/23/46/new-york-city-336475_960_720.jpg" 
+    img: <img src="https://forums-images.oneplus.net/attachments/536/536209-179509c69545aaad5662d0e0c7098363.jpg" 
     alt="Item Four">
     </img>
   }
