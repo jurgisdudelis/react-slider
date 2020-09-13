75956f4af9ca0231373c42da09e1cbd7ca0939e0
jurgisdudelis <63905386+jurgisdudelis@users.noreply.github.com>
2020-09-13 10:35:27 +0300

    Slide animation improvements

--- src/components/SliderActiveSlide.js
+++ src/components/SliderActiveSlide.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SliderActiveSlide = (props) => {
   return (
-    <div className={ props.index == props.activeIndex ? "slider__slide slider__slide--active": "slider__slide"}>
+    <div className={ props.index == props.activeIndex ? "slider__slide slider__slide--active" : "slider__slide"}>
       <p className="slider-slide__image"> { props.slide.img } </p>
-      <p className="slider-slide__content"> { props.slide.content } </p>
-      <p className="slider-slide__description"> { props.slide.description } </p>
+      <p className={ props.index == props.activeIndex ? "slider-slide__content slider-slide__content--active" : "slider-slide__content"}> { props.slide.content } </p>
+      <p className={ props.index == props.activeIndex ? "slider-slide__description slider-slide__description--active" : "slider-slide__description"}> { props.slide.description } </p>
     </div>
   );
 }
